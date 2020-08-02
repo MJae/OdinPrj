069ab820c962ee7df5e5248f504b18ed4dbe1c93
MJae <jadvinculax2020@gmail.com>
2020-08-02 15:41:08 +0800

    Completed reverseString

---
+++ js_exercises/reverseString/reverseString.js
@@ -0,0 +1,12 @@
+const reverseString = function(userStr) {
+  let userStrLen = userStr.length;
+  let resultStr = "";
+
+  for (let i = userStrLen - 1; i >= 0; i--) {
+    resultStr += userStr.charAt(i);
+  }
+
+  return resultStr;
+}
+
+module.exports = reverseString
